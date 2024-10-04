7f7b66232b3af8bb08a232568f7fbb5eb235ce12
Andineers <andinirizky91@gmail.com>
2024-10-04 21:22:08 +0700

    commit

--- QuizApp/main.js
+++ QuizApp/main.js
@@ -70,12 +70,44 @@ function selectAnswer(answer) {
     const isCorrect = selectedBtn.dataset.correct === "true";
     if(isCorrect) {
         selectedBtn.classList.add("correct");
+        score++;
     } else {
         selectedBtn.classList.add("incorrect");
     }
-    Array.from(answerButton.children).forEach(button =>)
+    Array.from(answerButton.children).forEach(button => {
+        if(button.dataset.correct === "true") {
+            button.classList.add("correct");
+            } 
+            button.disabled = true;
+    });
+    nextButton.style.display = "block";
 }
 
+function showScore() {
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}`;
+    nextButton.innerHTML = "play again";
+    nextButton.style.display = "block";
+}
+
+
+
+function handleNextButton() {
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length) {
+        showQuestion();
+    } else {
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", ()=> {
+    if(currentQuestionIndex < questions.length) {
+        handleNextButton();
+    } else {
+        startQuiz();
+    }
+});
 
 // function selectAnswer(answer) {
 //     if (answer.correct) {
